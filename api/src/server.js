ea10927b93e0f44b55862174dc2d34afc3ac89d3
André Barcelos Michels de Mattos <112213284+abmMattos@users.noreply.github.com>
2024-05-20 11:12:21 -0300

    preparing routes file

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const cors = require("cors")
 
-
+const routes = require("./routes")
 
 const app = express()
 app.use(express.json())
 
 
+app.use(routes)
 app.use(cors)
 
 const PORT = 3333
