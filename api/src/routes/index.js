4f55ca03dbd09a8d2255d014afa8320e8e3b8bcc
LcsNunes <lucas.porfirio.nunes@gmail.com>
2024-05-24 16:45:00 -0300

    Create Controller and routes for Admin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const deliverymanRoutes = require("./deliveryman.routes")
+const adminRoutes = require("./admin.routes")
 
 const routes = Router()
 
 routes.use('/deliveryman', deliverymanRoutes)
+routes.use('/admin', adminRoutes)
 
 module.exports = routes
